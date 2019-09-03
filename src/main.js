e769c7ea3f39d715465b3ea682a493d37e972403
Christopher Burge and Jason Huels <christopher+jason@>
2019-09-03 15:43:16 -0700

    add setinterval function to cycle through colors.

--- src/main.js
+++ src/main.js
@@ -3,3 +3,33 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
+
+
+
+
+
+
+
+
+  $("button").click(function() {
+    let simonArr = [];
+    for(let i=0; i<50; i++) {
+      simonArr.push(changeColor());
+    }
+
+    let i = 0;
+      setInterval(function() {
+        $("body").css("background-color", simonArr[i]); i++;
+      }, 1000);
+
+
+  });
+
+
+
+function changeColor() {
+  const COLORS = ["red", "blue", "green", "yellow"];
+  let rand = Math.floor(Math.random()*COLORS.length);
+
+  return COLORS[rand];
+}
